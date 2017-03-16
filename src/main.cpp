725437695dab0fe1b5e13308dd9778750668fa82
SelenCoin <selencoin@for-biz.com>
2017-03-16 14:38:41 +0300

    Fix orphan block sync

--- src/main.cpp
+++ src/main.cpp
@@ -964,7 +964,21 @@ uint256 WantedByOrphan(const CBlock* pblockOrphan)
 // miner's coin base reward
 int64_t GetProofOfWorkReward(int64_t nFees)
 {
+    // Make sure we don't lose fees due to 0 reward blocks.
+    if (pindexBest->nHeight > 500)
         return nFees;
+
+    int64_t nSubsidy = 0 * COIN;
+
+    if(pindexBest->nHeight < 100)
+        nSubsidy = 20000 * COIN;
+    else if(pindexBest->nHeight <= 500) // Make sure our chain is moving.
+        nSubsidy = 1 * COIN;
+
+    if (fDebug && GetBoolArg("-printcreation"))
+        printf("GetProofOfWorkReward() : create=%s nSubsidy=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nSubsidy);
+
+    return nSubsidy + nFees;
 }
 
 const int DAILY_BLOCKCOUNT  = 4320;
